1ea65092606cbbfbe3a6f1896c7b844101bfe122
Quang Thang <thang5a520057@gmail.com>
2026-09-09 14:14:16 +0700

    Hoan thanh va nop not B3

--- Buoi1/B3/User/src/main.c
+++ Buoi1/B3/User/src/main.c
@@ -15,12 +15,19 @@ void mdelay(volatile uint32_t s)
 
 int main(void)
 {
-    RCC_Enable_PortC();
+    RCC_Enable_PortA();
     RCC_Enable_PortB();
-    GPIO_Config(GPIOC, GPIO_PIN_13, GPIO_MODE_OUTPUT_PP);
-    GPIO_Config(GPIOB ,GPIO_PIN_0, GPIO_MODE_INPUT_PU);
+    GPIO_Config(GPIOA, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7 , GPIO_MODE_OUTPUT_PP);
+    GPIO_Config(GPIOB , GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_MODE_INPUT_FLOATING);
     while (1)
     {
-        GPIO_Write_Pin(GPIOC, GPIO_PIN_13, GPIO_Read_Pin(GPIOB, GPIO_PIN_0));
+        GPIO_Write_Pin(GPIOA, GPIO_PIN_0, !GPIO_Read_Pin(GPIOB, GPIO_PIN_8));
+        GPIO_Write_Pin(GPIOA, GPIO_PIN_1, !GPIO_Read_Pin(GPIOB, GPIO_PIN_9));
+        GPIO_Write_Pin(GPIOA, GPIO_PIN_2, !GPIO_Read_Pin(GPIOB, GPIO_PIN_10));
+        GPIO_Write_Pin(GPIOA, GPIO_PIN_3, !GPIO_Read_Pin(GPIOB, GPIO_PIN_11));
+        GPIO_Write_Pin(GPIOA, GPIO_PIN_4, !GPIO_Read_Pin(GPIOB, GPIO_PIN_12));
+        GPIO_Write_Pin(GPIOA, GPIO_PIN_5, !GPIO_Read_Pin(GPIOB, GPIO_PIN_13));
+        GPIO_Write_Pin(GPIOA, GPIO_PIN_6, !GPIO_Read_Pin(GPIOB, GPIO_PIN_14));
+        GPIO_Write_Pin(GPIOA, GPIO_PIN_7, !GPIO_Read_Pin(GPIOB, GPIO_PIN_15));
     }
 }
